615da0f9b4ddfee57b5051a14f5bf30061cbf6af
Gulam Faruk <golamfaruk204@gmail.com>
2021-10-27 17:08:18 +0600

    client section carousel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,4 +11,32 @@ $(document).ready(function () {
     $('.navbar-toggler').on("click", function () {
         $('.main-nav').slideToggle(500);
     });
+
+    // OWL CAROUSEL SLIDER 
+    $('.carosss').owlCarousel({
+        loop: true,
+        margin: 70,
+        center: true,
+        autoplay: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:1
+            },
+            1200:{
+                items:2
+            },
+        }
+
+    });
 })
